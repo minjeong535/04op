79eb06dc15f00f70d55a05863383246235493e13
minjeong535 <55127421+minjeong535@users.noreply.github.com>
2019-10-01 16:11:33 +0900

    4week 01, 02, 03

--- main.c
+++ main.c
@@ -4,16 +4,43 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-int x, y;
+{
+	int x, y, z, m;
+int a, b, c;
 
-printf("enter 2 integers :");
-scanf("%d %d", &x, &y);
+x=1;
+a=3;
+b=4;
+c=5;
 
-printf("plus is %i\n:", x+y);
-printf("minus is %i\n:", x-y);
-printf("multifly is %i\n", x*y);
-printf("division is %i\n", x/y);
-printf("na is %i\n", x%y);
+y=a*x*x+b*x+c;
+m=(x+y+z)/3;
+
+printf("y=%d, m=%d", y, m);
+}
+
+{
+	int x=3;
+	int cond1, cond2;
+	
+	cond1=2<x&&x<5;
+	cond2=x>1||x<4&&x>3;
+	
+	printf("cond1=%d, cond2=%d", cond1, cond2);
+}
+{
+	int x,y;
+	
+	printf("enter 2 integers :");
+	scanf("%d %d", &x, &y);
+	
+	printf("+ is %i\n :", x+y);
+	printf("- is %i\n :", x-y);
+	printf("* is %i\n :", x*y);
+	printf("/ is %i\n :", x/y);
+	printf("%% is %i\n", x%y);
+}
+
 
 	return 0;
 }
